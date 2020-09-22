0bbb5c6ab539fef8c3b184ae17c5d3acf96e8a17
ssensalo <edgaarayhan@gmail.com>
2020-09-22 20:32:52 +0300

    Adds camera object detection capability

---
+++ app/src/main/java/com/example/lawuna/DetectorActivity.java
@@ -0,0 +1,487 @@
+/*
+ * Copyright 2019 The Lawuna Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.lawuna;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.RectF;
+import android.graphics.Typeface;
+import android.media.ImageReader.OnImageAvailableListener;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.SystemClock;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Size;
+import android.util.TypedValue;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Timer;
+
+import com.example.lawuna.customview.OverlayView;
+import com.example.lawuna.customview.OverlayView.DrawCallback;
+import com.example.lawuna.env.BorderedText;
+import com.example.lawuna.env.CustomException;
+import com.example.lawuna.env.ImageUtils;
+import com.example.lawuna.env.Logger;
+import com.example.lawuna.tflite.Classifier;
+import com.example.lawuna.tflite.TFLiteObjectDetectionAPIModel;
+import com.example.lawuna.tracking.MultiBoxTracker;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.gson.Gson;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+/**
+ * An activity that uses a TensorFlowMultiBoxDetector and ObjectTracker to detect and then track
+ * objects.
+ */
+public class DetectorActivity extends CameraActivity implements OnImageAvailableListener {
+    private static final Logger LOGGER = new Logger();
+
+    // Configuration values for the prepackaged SSD model.
+    private static final int TF_OD_API_INPUT_SIZE = 300;
+    private static final boolean TF_OD_API_IS_QUANTIZED = true;
+    private static final String TF_OD_API_MODEL_FILE = "detect.tflite";
+    private static final String TF_OD_API_LABELS_FILE = "file:///android_asset/labelmap.txt";
+    private static final DetectorMode MODE = DetectorMode.TF_OD_API;
+    // Minimum detection confidence to track a detection.
+    private static final float MINIMUM_CONFIDENCE_TF_OD_API = 0.5f;
+    private static final boolean MAINTAIN_ASPECT = false;
+    private static final Size DESIRED_PREVIEW_SIZE = new Size(640, 480);
+    private static final boolean SAVE_PREVIEW_BITMAP = false;
+    private static final float TEXT_SIZE_DIP = 10;
+    private static final boolean SAME_DAY = true;
+    OverlayView trackingOverlay;
+    private Integer sensorOrientation;
+    private static final String BaseUrl = "<api-route>";
+
+    private Classifier detector;
+
+    private long lastProcessingTimeMs;
+    private Bitmap rgbFrameBitmap = null;
+    private Bitmap croppedBitmap = null;
+    private Bitmap cropCopyBitmap = null;
+
+    private boolean computingDetection = false;
+
+    private long timestamp = 0;
+
+    private Matrix frameToCropTransform;
+    private Matrix cropToFrameTransform;
+
+    private MultiBoxTracker tracker;
+
+    private static final int REQUEST_CAPTURE_IMAGE = 100;
+    private final int IMG_REQUEST = 1;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private Bitmap mImageBitmap;
+    private String mCurrentPhotoPath;
+    private ImageView mImageView;
+
+    private BorderedText borderedText;
+    final String BOUNDARY = "-------------" + System.currentTimeMillis();
+    private static final String IMAGE_DIRECTORY_NAME = "Lawuna";
+    private static File storageDir;
+    private static ArrayList<String> imageFileList = new ArrayList<String>();
+    private static ArrayList imageLogList = new ArrayList<String>();
+    private String imageUri;
+    private String images_captured;
+    private String logData;
+    private FusedLocationProviderClient fusedLocationClient;
+    private String currentLatitude  ;
+    private String currentLongitude;
+    private int locationRequestCode = 1000;
+    private String imagelog;
+    private double wayLatitude = 0.0, wayLongitude = 0.0;
+    private Gson gson = new Gson();
+    Timer timer = new Timer();
+    String yesterday = new SimpleDateFormat("dd").format(new Date());
+    @Override
+    public void onPreviewSizeChosen(final Size size, final int rotation) {
+        final float textSizePx =
+                TypedValue.applyDimension(
+                        TypedValue.COMPLEX_UNIT_DIP, TEXT_SIZE_DIP, getResources().getDisplayMetrics());
+        borderedText = new BorderedText(textSizePx);
+        borderedText.setTypeface(Typeface.MONOSPACE);
+
+        tracker = new MultiBoxTracker(this);
+
+        int cropSize = TF_OD_API_INPUT_SIZE;
+
+        try {
+            detector =
+                    TFLiteObjectDetectionAPIModel.create(
+                            getAssets(),
+                            TF_OD_API_MODEL_FILE,
+                            TF_OD_API_LABELS_FILE,
+                            TF_OD_API_INPUT_SIZE,
+                            TF_OD_API_IS_QUANTIZED);
+            cropSize = TF_OD_API_INPUT_SIZE;
+        } catch (final IOException e) {
+            e.printStackTrace();
+            LOGGER.e(e, "Exception initializing classifier!");
+            Toast toast =
+                    Toast.makeText(
+                            getApplicationContext(), "Classifier could not be initialized", Toast.LENGTH_SHORT);
+            toast.show();
+            finish();
+        }
+
+        previewWidth = size.getWidth();
+        previewHeight = size.getHeight();
+
+        sensorOrientation = rotation - getScreenOrientation();
+        LOGGER.i("Camera orientation relative to screen canvas: %d", sensorOrientation);
+
+        LOGGER.i("Initializing at size %dx%d", previewWidth, previewHeight);
+        rgbFrameBitmap = Bitmap.createBitmap(previewWidth, previewHeight, Config.ARGB_8888);
+        croppedBitmap = Bitmap.createBitmap(cropSize, cropSize, Config.ARGB_8888);
+
+        frameToCropTransform =
+                ImageUtils.getTransformationMatrix(
+                        previewWidth, previewHeight,
+                        cropSize, cropSize,
+                        sensorOrientation, MAINTAIN_ASPECT);
+
+        cropToFrameTransform = new Matrix();
+        frameToCropTransform.invert(cropToFrameTransform);
+
+        trackingOverlay = (OverlayView) findViewById(R.id.tracking_overlay);
+        trackingOverlay.addCallback(
+                new DrawCallback() {
+                    @Override
+                    public void drawCallback(final Canvas canvas) {
+                        tracker.draw(canvas);
+                        if (isDebug()) {
+                            tracker.drawDebug(canvas);
+                        }
+                    }
+                });
+
+        tracker.setFrameConfiguration(previewWidth, previewHeight, sensorOrientation);
+    }
+
+    @Override
+    protected void processImage() throws IOException, CustomException {
+        ++timestamp;
+        final long currTimestamp = timestamp;
+        trackingOverlay.postInvalidate();
+
+        // No mutex needed as this method is not reentrant.
+        if (computingDetection) {
+            readyForNextImage();
+            return;
+        }
+        computingDetection = true;
+        File imageFile = null;
+        try {
+         // Creates a temporary image file in cache
+            imageFile = createImageFile();
+            imageUri = String.valueOf(imageFile);
+        } catch (IOException ex) {
+            // Error occurred while creating the File
+            Toast.makeText(
+                    DetectorActivity.this,
+                    "Error occurred while creating the File",
+                    Toast.LENGTH_LONG)
+                    .show();
+        }
+
+        LOGGER.i("Preparing image " + currTimestamp + " for detection in bg thread.");
+        getPosition();
+        rgbFrameBitmap.setPixels(getRgbBytes(), 0, previewWidth, 0, 0, previewWidth, previewHeight);
+        if (imageFile != null) {
+        //Save the image to the server
+            connectServer(imageFile);
+        } else {
+            LOGGER.i("NO PHOTO CONTAINED ");
+        }
+        LOGGER.i("DIRECTORY " + imageFile);
+        readyForNextImage();
+
+        final Canvas canvas = new Canvas(croppedBitmap);
+        canvas.drawBitmap(rgbFrameBitmap, frameToCropTransform, null);
+        // For examining the actual TF input.
+        if (SAVE_PREVIEW_BITMAP) {
+            ImageUtils.saveBitmap(croppedBitmap);
+        }
+
+        runInBackground(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        LOGGER.i("Running detection on image " + currTimestamp);
+                        final long startTime = SystemClock.uptimeMillis();
+                        final List<Classifier.Recognition> results = detector.recognizeImage(croppedBitmap);
+                        lastProcessingTimeMs = SystemClock.uptimeMillis() - startTime;
+
+                        cropCopyBitmap = Bitmap.createBitmap(croppedBitmap);
+                        final Canvas canvas = new Canvas(cropCopyBitmap);
+                        final Paint paint = new Paint();
+                        paint.setColor(Color.RED);
+                        paint.setStyle(Style.STROKE);
+                        paint.setStrokeWidth(2.0f);
+
+                        float minimumConfidence = MINIMUM_CONFIDENCE_TF_OD_API;
+                        switch (MODE) {
+                            case TF_OD_API:
+                                minimumConfidence = MINIMUM_CONFIDENCE_TF_OD_API;
+                                break;
+                        }
+
+                        final List<Classifier.Recognition> mappedRecognitions =
+                                new LinkedList<Classifier.Recognition>();
+
+                        for (final Classifier.Recognition result : results) {
+                            final RectF location = result.getLocation();
+                            if (location != null && result.getConfidence() >= minimumConfidence) {
+                                canvas.drawRect(location, paint);
+
+                                cropToFrameTransform.mapRect(location);
+
+                                result.setLocation(location);
+                                mappedRecognitions.add(result);
+                            }
+                        }
+
+                        tracker.trackResults(mappedRecognitions, currTimestamp);
+                        trackingOverlay.postInvalidate();
+
+                        computingDetection = false;
+
+                        runOnUiThread(
+                                new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        showFrameInfo(previewWidth + "x" + previewHeight);
+                                        showCropInfo(cropCopyBitmap.getWidth() + "x" + cropCopyBitmap.getHeight());
+                                        showInference(lastProcessingTimeMs + "ms");
+                                    }
+                                });
+                    }
+                });
+    }
+
+
+    //  Query for all the activities in the device which will handle the CAPTURE_REQUEST intent
+    private void openCameraIntent() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (cameraIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                LOGGER.i("IOException" + ex);
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+
+    private void openCamera() {
+        Intent pictureIntent = new Intent(
+                MediaStore.ACTION_IMAGE_CAPTURE
+        );
+        startActivityForResult(pictureIntent, REQUEST_CAPTURE_IMAGE);
+    }
+
+    //  Load image
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100 && data != null) {
+            Uri path = data.getData();
+        // Get Capture Image
+            mImageBitmap = (Bitmap) data.getExtras().get("data");
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "IMG_" + timeStamp + "_";
+        storageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), IMAGE_DIRECTORY_NAME);
+        // Create the storage directory if it does not exist
+        if (!storageDir.exists()) {
+            LOGGER.i(">>HERE!!!");
+            storageDir.mkdirs();
+            if (!storageDir.mkdirs()) {
+                LOGGER.i("Unable to create directory.");
+                return null;
+            }
+        }
+        // missing image.
+        File image = File.createTempFile(
+                imageFileName,  // prefix
+                ".jpg",         // suffix
+                storageDir      // directory
+        );
+        try {
+            FileOutputStream out = new FileOutputStream(image);
+            // Compress Image captured
+            croppedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
+    public void setSentDate(String date){this.sent_date=date;}
+    public String getSentDate() {return sent_date;}
+
+
+    void connectServer(File imageUrl) {
+        setSentDate(sent_date);
+        RequestBody postBodyImage = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("phonenumber", getPhoneNumber())
+                .addFormDataPart("latitude", getLatitude())
+                .addFormDataPart("longitude", getLongitude())
+                .addFormDataPart("date", sent_date)
+                .addFormDataPart("photo", imageUrl.getName(),
+                        RequestBody.create(MediaType.parse("image/JPEG"), imageUrl))
+                .build();
+
+        Toast.makeText(
+                DetectorActivity.this,
+                "Please wait ...",
+                Toast.LENGTH_LONG)
+                .show();
+
+        postRequest(BaseUrl, postBodyImage);
+    }
+
+    public void postRequest(String postUrl, RequestBody postBody) {
+        Request request = new Request.Builder()
+                .url(postUrl)
+                .post(postBody)
+                .build();
+
+        OkHttpClient client = obj.client_conn.getHttpClient();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                // Cancel the post on failure.
+                call.cancel();
+
+                // In order to access the TextView inside the UI thread, the code is executed inside runOnUiThread()
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(
+                                DetectorActivity.this,
+                                "Failed to Connect to Server",
+                                Toast.LENGTH_LONG)
+                                .show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                // In order to access the TextView inside the UI thread, the code is executed inside runOnUiThread()
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Delete the file from the internal storage.
+                        try {
+                            deleteImageFiles();
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                        }
+                        Toast.makeText(
+                                DetectorActivity.this,
+                                "Image Saved",
+                                Toast.LENGTH_LONG)
+                                .show();
+                    }
+                });
+                response.body().close();
+            }
+        });
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.tfe_od_camera_connection_fragment_tracking;
+    }
+
+    @Override
+    protected Size getDesiredPreviewFrameSize() {
+        return DESIRED_PREVIEW_SIZE;
+    }
+
+    // Which detection model to use: by default uses Tensorflow Object Detection API frozen
+    // checkpoints.
+    private enum DetectorMode {
+        TF_OD_API;
+    }
+
+    @Override
+    protected void setUseNNAPI(final boolean isChecked) {
+        runInBackground(() -> detector.setUseNNAPI(isChecked));
+    }
+
+    @Override
+    protected void setNumThreads(final int numThreads) {
+        runInBackground(() -> detector.setNumThreads(numThreads));
+    }
+}
--- app/src/main/java/com/example/lawuna/ObjectDetectorActivity.java
+++
@@ -1,4 +0,0 @@
-package com.example.lawuna;
-
-public class ObjectDetectorActivity {
-}
